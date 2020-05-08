439c3b79cfb0011fecacac7046f6122d34e8f685
ochimiru <h.oikawa0730@gmail.com>
2020-05-08 12:08:49 +0900

    320行目まで

--- one_hour_dungeon.py
+++ one_hour_dungeon.py
@@ -182,6 +182,153 @@ def put_event():
     # 宝箱と繭の配置
     for i in range(60):
         x = random.randint(3, DUNGEON_W-4)
+        y = random.randint(3, DUNGEON_H-4)
+        if dungeon[y][x] == 0:
+            dungeon[y][x] = random.choice([1, 2, 2, 2, 2)]
 
-#-------------------184行目まで----------------------------
+    # プレイヤーの初期位置
+    while True:
+        pl_x = random.randint(3, DUNGEON_W-4)
+        pl_y = random.randint(3, DUNGEON_H-4)
+        if dungeon[pl_y][pl_x] == 0:
+            break
+    
+    pl_d = 1
+    pl_a = 2
 
+# 主人公の移動
+def move_player(key):
+    global idx, tmr, pl_x, pl_y, pl_d, pl_a, pl_life, food, potion, blazegem, treasure
+
+    if dungeon[pl_y][pl_x] == 1 #宝箱に乗った
+        dungeon[pl_y][pl_x] = 0
+        treasure = random.choice([0,0,0,1,1,1,1,1,1,2])
+        if treasure = 0:
+            potion += 1
+        if treasure = 1:
+            blazegem += 1
+        if treasure = 2:
+            food = int(food/2)
+        idx = 3
+        tmr = 0
+        return
+    if dungeon[pl_y][pl_x] == 2: # 繭に乗った
+        dungeon[pl_y][pl_x] = 0
+        r = random.randint(0, 99)
+        if r < 40: # 食料
+            treasure = random.choice([3,3,3,4])
+            if treasure == 3:
+                food += 20
+            if treasure == 4:
+                food += 100
+            idx = 3
+            tmr = 0
+        else: # 敵出現
+            idx = 10
+            tmr = 0
+        return
+    if dungeon[pl_y][pl_x] == 3: # 階段に乗った
+        idx = 2
+        tmr = 0
+        return
+
+    # 方向キーで上下左右に移動
+    x = pl_x
+    y = pl_y
+    if key[K_UP] == 1:
+        pl_d = 0
+        if dungeon[pl_y-1][pl_x] != 9:
+            pl_y -= 1
+    if key[K_DOWN] == 1:
+        pl_d = 1
+        if dungeon[pl_y+1][pl_x] != 9:
+            pl_y += 1
+    if key[K_LEFT] == 1:
+        pl_d = 2
+        if dungeon[pl_y][pl_x-1] != 9:
+            pl_x -= 1
+    if key[K_RIGHT] == 1:
+        pl_d = 3
+        if dungeon[pl_y][pl_x+1] != 9:
+            pl_x += 1
+    pl_a = pl_d * 2
+    if pl_x != x or pl_y != y: # 移動したら食料の量と体力を計算
+        pl_a += tmr%2
+        if food > 0:
+            food -= 1
+            if pl_life < pl_lifemax:
+                pl_life += 1
+        else:
+            pl_life -= 5
+            if pl_life <= 0:
+                pl_life = 0
+                pygame.mixer.music.stop()
+                idx = 9
+                tmr = 0
+
+# 影付き文字の表示
+def draw_text(bg, txt, x, y, fnt, col):
+    sur = fnt.render(txt, True, BLACK)
+    bg.blit(sur, [x+1, y+2])
+    sur = fnt.render(txt, True, col)
+    bg.blit(sur, [x, y])
+
+# 主人公の能力を表示
+def draw_para(bg, fnt):
+    X = 30
+    Y = 600
+    bg.blit(imgPara, [X, Y])
+    col = WHITE
+    if pl_life < 10 and tmr%2 == 0:
+        col = RED
+    draw_text(bg, " {}/{}".format(pl_life, pl_lifemax), X+128, Y+6, fnt, col)
+    draw_text(bg, str(pl_str), X+128, Y+33, fnt, WHITE)
+    col = WHITE
+    if food == 0 and tmr%w == 0:
+        col = RED
+    draw_text(bg, str(food), X+128, Y+60, fnt, col)
+    draw_text(bg, str(potion), X+266, Y+6, fnt, WHITE)
+    draw_text(bg, str(blazegem), X+266, Y+ 33, fnt, WHITE)
+
+# 戦闘に入る準備をする
+def init_battle():
+    global imgEnemy, emy_name, emy_lifemax, emy_life, emy_str, emy_x, emy_y
+    typ = random.randint(0, floor)
+    if floor >= 10:
+        typ = random.randint(0, 9)
+    lev = random.randint(1, floor)
+    imgEnemy = pygame.image.load("image/enemy" + str(typ) + ".png")
+    emy_name = EMY_NAME[typ] + "LV" + str(lev)
+    emy_lifemax = 60*(typ+1) + (lev-1)*10
+    emy_life = emy_lifemax
+    emy_str = int(emy_lifemax/8)
+    emy_x = 440-imgEnemy.get_width()/2
+    emy_y = 560-imgEnemy.get_height()
+
+# 敵の体力を表示するバー
+def draw_bar(bg, x, y, w, h, val, ma):
+    pygame.draw.rect(bg, WHITE, [x-2, y-2, w+4, h+4])
+    pygame.draw.rect(bg, BLACK, [x, y, w, h])
+    if val > 0:
+        pygame.draw.rect(bg, (0, 128, 255), [x, y, w*val/ma, h])
+
+# 戦闘画面の描写
+def draw_battle(bg, fnt):
+    global emy_blink, dmg_eff
+    bx = 0
+    by = 0
+    if dmg_eff > 0:
+        dmg_eff -= 1
+        bx = random.randint(-20, 20)
+        by = random.randint(-10, 10)
+    bg.blit(imgBtlBG, [bx, by])
+    if emy_life > 0 and emy_blink%2 == 0:
+        bg.blit(imgEnemy, [emy_x, emy_y+emy_step])
+    draw_bar(bg, 340, 580, 200, 10, emy_life, emy_lifemax)
+    if emy_blink > 0:
+        emy_blink -= 1
+    for i in range(10): # 戦闘メッセージの表示
+        draw_text(bg, message[i], 600, 100+i*50, fnt, WHITE)
+        draw_para(bg, fnt) # 主人公の能力を表示
+
+# コマンドの入力と表示
